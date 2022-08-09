780eea0bb4d9606e2ca074e375028317a124c6ed
elizabeth.goullaud <egoullaud@gmail.com>
2022-08-09 12:31:54 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var numberString = "12";
+var numberValue = 12;
+
+if (numberString === numberValue) {
+  console.log("the values and types are equal");
+} else if (numberString == numberValue) {
+  console.log("Only the values are equal.");
+} else {
+  console.log("Neither the values or types are equal.");
+}
